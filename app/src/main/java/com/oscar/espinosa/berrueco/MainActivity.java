4a6519bdf35a1325f4860f38de2a83bbeb25a0de
OSD4R <espinosa.berrueco.oscar.manuel@gmail.com>
2021-11-08 11:51:20 -0600

    Se agrego metodo, actividad para la suma'

--- app/src/main/java/com/oscar/espinosa/berrueco/MainActivity.java
+++ app/src/main/java/com/oscar/espinosa/berrueco/MainActivity.java
@@ -6,9 +6,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonSuma;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSuma = findViewById(R.id.buttonSuma);
+        buttonSuma.setOnClickListener(new view.OnClickListener() {
+            @Override
+            public void onClic(View v) {
+                abrirSuma()
+            }
+        });
     }
+    private void abrirSuma()
+
+    private Object packageContext;
+    Intent intent = new Intent(packageContext: this , SumaActivity.class  );
+    startActivity (intent) ;
 }
